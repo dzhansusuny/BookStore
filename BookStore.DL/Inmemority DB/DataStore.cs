63c9de4e2ed75ba8215fd9a60ed102599a0b77fe
cnssny <asdfghjkl@abv.bg>
2023-02-23 19:14:38 +0200

    c gfc

---
+++ BookStore.DL/Inmemority DB/DataStore.cs
@@ -0,0 +1,25 @@
+﻿
+
+using BookStore.Models.data;
+
+namespace BookStore.DL.Inmemority_DB
+{
+    public static class DataStore
+    {
+        public static List<Author> Authors { get; set; }
+        = new List<Author>()
+        {
+            new Author()
+            {
+                Id = 1, 
+                Name ="Gosho",
+                Bio = "Gosho bio"
+            },
+             new Author()
+            {
+                Id = 2,
+                Name ="Ginka",
+                Bio = "Ginka bio"
+        }
+    }
+}
---
+++ BookStore.Models/data/Costumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.data
+{
+    internal class costumer : Person
+    {
+        public int costumerId { get; set; }
+        public string costumerName { get; set; }
+        public string Phonenumber { get; set; } 
+
+    }
+}
